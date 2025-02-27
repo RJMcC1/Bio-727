0ddab432a2840ad567b8523bd6ba90c8bc5b1ad2
gaendrit <gashien7@gmail.com>
2025-02-27 23:12:55 +0000

    Create image grabber code for app.py
    works with the populationv2.html page

---
+++ image grabber code for app.py
@@ -0,0 +1,18 @@
+@app.route("/population/<population_name>")
+def population_page(population_name):
+    """Serve the population details page with image and information."""
+    # Fetch the image associated with the population from the database
+    results = query_database("""
+        SELECT i.image
+        FROM images i
+        WHERE i.population = ?
+    """, (population_name,))
+
+    print(f"Results for population '{population_name}': {results}")
+    
+    if results and results[0][0]:
+        image_data=results[0][0]
+        encoded_image = base64.b64encode(image_data).decode('utf-8')
+        return render_template("population.html", population_name=population_name, encoded_image=encoded_image)
+    else:
+        return render_template("population.html", population_name=population_name, encoded_image=None)
